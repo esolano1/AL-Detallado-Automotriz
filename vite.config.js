5b9171cc7f76746b10e916a43cea0ba5878d036e
esolano1 <joeybello23@gmail.com>
2026-01-21 00:13:08 -0600

    feat: changed vite.config.js to fix paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-base: "/AL-Detallado-Automotriz/",
+  base: "/AL-Detallado-Automotriz/",
   plugins: [tailwindcss()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        services: resolve(__dirname, "services.html"),
+      },
+    },
+  },
+});
