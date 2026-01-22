1ac324b05a16bea4d6c54fecfea3bebed69bbb60
esolano1 <joeybello23@gmail.com>
2026-01-22 00:04:38 -0600

    feat: add  contact and location page

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import './style.css'
 
+const btn = document.getElementById("mobileMenuBtn")
+const menu = document.getElementById("mobileMenu")
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden")
+})
